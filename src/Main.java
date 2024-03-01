3ee18d1dc0bca5b6a38264d86a9f5f420c663032
Alexander Khyrbu <rjrednaxela@vk.ru>
2024-03-01 13:50:09 +0300

    rename variable

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 100; // Остаток на счёте
-        int y = 1000; // Пополнение
+        int remains = 100; // Остаток на счёте
+        int refill = 1000; // Пополнение
         int z = 0; // Бонусы на счёте
 
         int bonus;
         int outcome;
-        if (y >= 1000) {
-            bonus = y / 100; // Расчёт бонуса с пополнения
-            outcome = x + y + bonus; // Общая сумма
+        if (refill >= 1000) {
+            bonus = refill / 100; // Расчёт бонуса с пополнения
+            outcome = remains + refill + bonus; // Общая сумма
             System.out.println("Итоговая сумма на счету: " + outcome);
             System.out.printf("Из них бонусов: " + bonus);
         } else {
 
             bonus = 0;
-            outcome = x + y + bonus; // Общая сумма
+            outcome = remains + refill + bonus; // Общая сумма
 
             System.out.println("Итоговая сумма на счету: " + outcome);
             System.out.printf("Из них бонусов: " + z);
